43b32521e75b413058a2ff2562b8edb1fd70941e
argadhana <yoshspecta333@gmail.com>
2020-06-24 19:44:01 +0700

    master

---
+++ resources/views/anggota/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.wrapper')
+@section('title', 'Dashboard')
+@section('content')
+<div class="content-page">
+<!-- Start content -->
+
+<!-- END content -->
+</div>
+<!-- END content-page -->
+@endsection
